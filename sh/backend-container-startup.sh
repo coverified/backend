348e329e829351d21751b09e4e9c59584726979f
Johannes Hiry <johannes.hiry@tu-dortmund.de>
2020-03-30 08:39:41 +0200

    added docker startup scripts

---
+++ sh/backend-container-startup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# fail on first error
+set -e
+echo "> Running start script..."
+
+echo "> Wait for database on $DATABASE_SERVER to be ready..."
+
+/wait-for-it.sh $DATABASE_SERVER:5432 --timeout=120
+
+echo "> database seems to be ready..."
+
+echo "> Migrating database model..."
+
+cd /app
+python manage_db.py db init
+python manage_db.py db migrate
+python manage_db.py db upgrade
+
+echo "> Migration successful"
+
+echo "> Starting backend ..."
+
+python main.py
